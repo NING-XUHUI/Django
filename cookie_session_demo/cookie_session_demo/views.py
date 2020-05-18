572f3195b1826c97cfd5fca0bb3ec8c4d79ce4b1
NING-XUHUI <892897986@qq.com>
2020-05-18 21:38:47 +0800

    django

--- cookie_session_demo/cookie_session_demo/views.py
+++ cookie_session_demo/cookie_session_demo/views.py
@@ -13,3 +13,16 @@ def index(request):
     expires = datetime(year=2020, month=5, day=18, hour=0, minute=9, second=9)
     response.set_cookie('username', 'zhiliao', max_age=180)
     return response
+
+
+def session_view(request):
+    # request.session['username'] = 'zhiliao'
+    # username = request.session.get('username')
+    # username = request.session.get('username')
+    # request.session['username'] = 'zhiliao'
+    # request.session['userid'] = 10
+    # request.session.clear()
+    username = request.session.get('username')
+    print(username)
+    return HttpResponse("session_view")
+
